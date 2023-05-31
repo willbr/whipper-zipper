afce2b50c92f1d1b33ed7f93c54fcc66945e98a3
William Bettridge-Radford <william.bettridge.radford@gail.com>
2023-05-31 21:42:38 +0100

    wip range lookup

--- tk-grid.py
+++ tk-grid.py
@@ -72,11 +72,15 @@ def is_number(s):
         return True
     except ValueError:
         pass
+    except TypeError:
+        pass
 
     try:
         i = int(s)
         return True
     except ValueError:
+        pass
+    except TypeError:
         pass
 
     return False
@@ -84,26 +88,77 @@ def is_number(s):
 #print(is_number('3.14'))
 #print(is_number('10'))
 
-def resolve_range_refs(s):
-    matched = re.match(r'\w+:\w+', s)
-    if not matched:
-        return s
-    ref = matched.group()
-    return s
+def to_rc(s):
+    s = s.strip().lower()
+    assert len(s) == 2
+    col = ord('a') - ord(s[0]) + 1
+    row = int(s[1]) 
+    return (row, col)
 
-i = 'a1:a1'
+i = 'a1'
+er = (1, 1)
+r = to_rc(i)
+if r != er:
+    print(f'{i=}, {er=}, {r=}')
+    assert False
+
+def cellname(r, c):
+    lhs = chr(ord('a') + c - 1)
+    return f'{lhs}{r}'
+
+i = (1, 1)
 er = 'a1'
-r = resolve_range_refs(i)
+r = cellname(*i)
 if r != er:
     print(f'{i=}, {er=}, {r=}')
     assert False
 
-i = 'a1:a3'
-er = '[a1,a2,a3]'
-r = resolve_range_refs(i)
+def resolve_range_refs(s):
+    matched = re.search(r'(\w+):(\w+)', s)
+    if not matched:
+        #print(f'match failed: {s}')
+        return s
+    lhs, rhs = matched.groups()
+    r1, c1 = to_rc(lhs)
+    r2, c2 = to_rc(rhs)
+    #print((r1,c1), (r2,c2))
+    r1, r2 = sorted((r1, r2))
+    c1, c2 = sorted((c1, c2))
+    cells = []
+    for c in range(c1, c2+1):
+        for r in range(r1, r2+1):
+            cn = cellname(r, c)
+            cells.append(cn)
+    #print(cells)
+    ref = matched.group()
+    new_ref = '[' + (', '.join(cells)) + ']'
+    new_s = s.replace(ref, new_ref)
+    return new_s
+
+i  = 'sum(a1:a3)'
+er = 'sum([a1, a2, a3])'
+r  = resolve_range_refs(i)
+
 if r != er:
     print(f'{i=}, {er=}, {r=}')
     assert False
+
+i  = 'a1:a2'
+er = '[a1, a2]'
+r  = resolve_range_refs(i)
+
+if r != er:
+    print(f'{i=}, {er=}, {r=}')
+    assert False
+
+i  = 'a1:a3'
+er = '[a1, a2, a3]'
+r  = resolve_range_refs(i)
+
+if r != er:
+    print(f'{i=}, {er=}, {r=}')
+    assert False
+
 
 def on_leave(event):
     widget = event.widget
@@ -120,11 +175,13 @@ def on_leave(event):
     orig_expr = expr
 
     expr = resolve_range_refs(expr)
+    print(expr)
 
     for i in range(10):
         try:
             new_value = eval(expr)
             failed = False
+            break
         except SyntaxError as se:
             print(se.args)
             return
